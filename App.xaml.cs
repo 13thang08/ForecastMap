de7a372e3cb8cbe2bce75452191cdb26f68853f5
Vu Manh Thang <thang@zyyx.jp>
2015-02-18 17:08:36 +0900

    todayForecastView

--- App.xaml.cs
+++ App.xaml.cs
@@ -118,7 +118,7 @@ namespace ForecastMap
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                rootFrame.Navigate(typeof(FavoritesList), e.Arguments);
+                rootFrame.Navigate(typeof(DetailForecast), e.Arguments);
             }
             // Ensure the current window is active
             Window.Current.Activate();
